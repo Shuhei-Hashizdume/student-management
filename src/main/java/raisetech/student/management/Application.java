7b8c0411c4c8a8920181cbcd78ae3fc37258488f
Shuhei <32soccer1121@gmail.com>
2025-09-10 23:02:32 +0900

    最新分に変更する

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -1,21 +1,32 @@
 package raisetech.student.management;
 
-import org.apache.commons.lang3.StringUtils;
+import java.util.List;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+
 @RestController
 public class Application {
+
+	@Autowired
+	private StudentRepository repository;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	@GetMapping("hello")
-	public String hello(){
-		return "Hello world!";
+	@GetMapping("studentList")
+	public List<Student> getStudentList(){
+		 return repository.search();
 	}
+
+
+
 
 }
---
+++ src/main/java/raisetech/student/management/Student.java
@@ -0,0 +1,19 @@
+package raisetech.student.management;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Student {
+
+ @Getter
+ @Setter
+
+ private String id;
+ private String fullName;
+ private String furigana;
+ private String nickname;
+ private String emailAddress;
+ private String area;
+ private int age;
+ private String gender;
+}
